a81e3ba7f9fbfcea9c582561dfb941c74bffca3c
Iswanul Umam <iswanul.umam@gmail.com>
2023-09-20 10:53:37 +0700

    add problem4

---
+++ problem4/main.py
@@ -0,0 +1,10 @@
+def ubah_huruf(sentence):
+    pattern = ""
+    return pattern
+
+if __name__ == '__main__':
+    print(ubah_huruf("SEPULSA OKE")) # COZEVCK YUO
+    print(ubah_huruf("ALTERRA ACADEMY")) # KVDOBBK KMKNOWI
+    print(ubah_huruf("INDONESIA")) # SXNYXOCSK
+    print(ubah_huruf("GOLANG")) # QYVKXQ
+    print(ubah_huruf("PROGRAMMER")) # ZBYQBKWWOB
