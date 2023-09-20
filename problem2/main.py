968671f7347dc9202b22391dba647cdb3a8b1e85
Iswanul Umam <iswanul.umam@gmail.com>
2023-09-20 10:32:58 +0700

    add problm2

---
+++ problem2/main.py
@@ -0,0 +1,21 @@
+def draw_xyz(N):
+    pattern = ""
+    return pattern
+
+if __name__ == '__main__':
+    print(draw_xyz(3))
+    """
+    Y Z X
+    Z Y X
+    Y Z X
+    """
+
+
+    print(draw_xyz(5))
+    """
+    Y Z X Z Y
+    X Y Z X Z
+    Y X Y Z X
+    Z Y X Y Z
+    X Z Y X Y
+    """
